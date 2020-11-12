77dadc0096bd9e5083423dc64e133c3e77855e4b
Gamer745 <clubblock1@gmail.com>
2020-11-11 21:16:01 -0600

    Implemented redux

--- backend/server.js
+++ backend/server.js
@@ -6,25 +6,22 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-import products from './data/products.js';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 connectDB();
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(product => product._id === req.params.id);
-
-    res.json(product);
-});
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.green.bold);
